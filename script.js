c83b81a09c6905659d9dc24238af7333fcc48f5f
julianaoaguilar <julianaoaguilar16@gmail.com>
2025-08-21 09:51:55 -0300

    :sparkles: feat: Render tweets

--- script.js
+++ script.js
@@ -1,14 +1,52 @@
 const newPostButton = document.getElementById('newPostButton');
 const newPostInput = document.getElementById('newPostInput');
 const sendPostButton = document.getElementById('addNewPostButton');
+const timeline = document.getElementById('timeline');
+
+// Sample data for the xinfeed
+const xinfeed = {
+    posts: [
+        { user: 'Xinlana', content: 'Meu primeiro post :)', date: '21/08/2025 13:30' },
+        { user: 'Xinlana', content: 'Meu segundo post :)', date: '21/08/2025 13:30' }
+    ]
+};
+
+// create a post element
+function createPostElement(post) {
+    
+    const postContainer = document.createElement('article');
+    postContainer.style.position = 'relative';
+
+    const postText = document.createElement('p');
+    postText.textContent = post.content;
+
+    postContainer.append(postText);
+    return postContainer;
+}
 
 
-// Function to toggle the visibility of the new post form
+// render tweets in the timeline
+function renderPosts() {
+    timeline.innerHTML = '';
+    xinfeed.posts.forEach(post => {
+        timeline.appendChild(createPostElement(post));
+    });
+}
+
+
+
+// toggle the visibility of the new post form
 function togglePostForm(show) {
     newPostInput.hidden = !show;
     sendPostButton.hidden = !show;
     if (show) newPostInput.value = '';
 }
 
+
+
+// when loaded, render the posts
+window.addEventListener('load', renderPosts);
+
 // Event listener for the send post button
 newPostButton.addEventListener('click', () => togglePostForm(true));
+
